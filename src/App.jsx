4922eb6c377cc9a58cdf7d89b67b373c1dea0115
GemelliAngelo <gemeangelo27@gmail.com>
2024-12-05 15:26:40 +0100

    add fetchPosts

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     fetch("http://127.0.0.1:3000/posts")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setPosts(() => [...data]);
       });
   };
 
